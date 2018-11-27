f5967bbc044e08d0ee952a15572f3eadb387cff7
bill <bill.hao@voyageone.cn>
2018-11-27 19:25:02 +0800

    添加： 添加一个test controller

---
+++ spring_cloud_demo/spring_cloud_demo_client_001/src/main/java/com/hdd/cloud/demo/client001/controller/HelloControllerImpl.java
@@ -0,0 +1,19 @@
+package com.hdd.cloud.demo.client001.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * controller
+ *
+ * @author bill.hao
+ * @create 2018/11/27 下午 7:19
+ */
+@RestController
+public class HelloControllerImpl {
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable(value = "name") String name){
+        return "hello"+name;
+    }
+}
